8e9e7b0218827e8aa25b59f47b622d62304af74e
Dwight Scott <scott.1379@osu.edu>
2014-10-15 14:20:03 -0400

    updated for rails 4 and latest version of devise

--- app/controllers/devise/shibboleth_sessions_controller.rb
+++ app/controllers/devise/shibboleth_sessions_controller.rb
@@ -1,15 +1,13 @@
 class Devise::ShibbolethSessionsController < Devise::SessionsController
   unloadable
-  
   def new
-    resource = build_resource
+    #resource = build_resource
     shib_config = YAML.load(ERB.new(File.read(::Devise.shibboleth_config || "#{Rails.root}/config/shibboleth.yml")).result)[Rails.env]
 
     destination = request.protocol
     destination << request.host
     destination << ":#{request.port.to_s}" unless request.port == 80
     destination << after_sign_in_path_for(resource)
-    
     shib_login_url = shib_config['shibb_login_url'] + "?target=" + URI.escape(destination, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
 
     redirect_to(shib_login_url)
--- lib/devise_shibboleth_authenticatable/routes.rb
+++ lib/devise_shibboleth_authenticatable/routes.rb
@@ -1,10 +1,9 @@
 ActionDispatch::Routing::Mapper.class_eval do
   protected
-  
   def devise_shibboleth_authenticatable(mapping, controllers)
     resource :session, :only => [], :controller => controllers[:shibboleth_sessions], :path => "" do
       get :new, :path => mapping.path_names[:sign_in], :as => "new"
-      match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy"
+      get :destroy, :path => mapping.path_names[:sign_out], :as => "destroy"
     end
   end
 end
